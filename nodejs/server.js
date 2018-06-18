446a0ff811fa35b100716aa3bd9db313552ba2a8
Daniel Saltz <dsaltz@u.rochester.edu>
2018-06-17 19:54:46 -0500

    changes after putting example code on aws ec2

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,18 +1,11 @@
-/*
-Send To:
-
-zzoutop@gmail.com
-valentine.216@osu.edu
-
-*/
-
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(express.static('static_files'));
+// app.use(express.static('static_files'));
+app.use(express.static(__dirname + '/public'));
+
 
 // importing sqlite and creating the database
 var sqlite3 = require("sqlite3").verbose();
@@ -34,9 +27,11 @@ db.serialize(function() {
 ************************************************/
 
 	// "Log In" clicks
-	app.get('/users/*', function (req, res) {
+	app.get('/login', function (req, res) {
 		var myUsername = req.query.username;
 		var myPassword = req.query.password;
+		console.log("Log In attempt..." + myUsername + ", " + myPassword);
+
 		if (!myUsername && !myPassword){ res.send("Enter the required fields");}
 		else if (!myUsername){res.send("Enter your username");}
 		else if (!myPassword){res.send("Enter your password");}
@@ -44,10 +39,7 @@ db.serialize(function() {
 			db.all("SELECT * FROM users WHERE username = ?" +
 			" AND password = ?", [myUsername,myPassword], function(err,row){
 				if (row.length < 1){ // checks for empty list
-					res.send("Invalid information, "+
-					 "or you have not yet created an account. To create a "+ 
-					 "new account, enter a username and password, "+
-					 "and click Create New Account.");	
+					res.send("Invalid credentials!");	
 				} else {
 					res.send("Logged in successfully!");
 				}
@@ -56,14 +48,12 @@ db.serialize(function() {
 	});
 
 	// "Sign Up" clicks
-	app.post('/users', function (req, res) {
-
-		console.log(req.body);
-		console.log(res.body);
-
+	app.post('/signup', function (req, res) {
 		var postBody = req.body;
 		var myUsername = postBody.username;
 		var myPassword = postBody.password;
+
+		console.log("Sign Up attempt..." + myUsername + ", " + myPassword);
 
 		if (!myUsername && !myPassword){ res.send("Enter the required fields");}
 		else if (!myUsername){ res.send("Enter your username");}
@@ -72,16 +62,25 @@ db.serialize(function() {
 			if (myPassword.length < 7){
 				res.send("Your password must be longer than 7 characters");
 			} else {	
-				db.run("INSERT INTO users VALUES (?,?)",[myUsername,myPassword], function(err, row) {
-					if (err != null){ res.send("Failed to create new account");}
-					else { res.send("Created a new account!");}
-				});	
+				// first check if that username exists in the table already
+				db.all("SELECT * FROM users WHERE username = ?", [myUsername], function(err,row){
+					if (row.length < 1){ // checks for empty list
+						db.run("INSERT INTO users VALUES (?,?)",[myUsername,myPassword], function(err, row) {
+							if (err != null){ res.send("Failed to create new account");}
+							else { res.send("Created a new account!");}
+						});	
+					} else {
+						res.send("That username is already taken!");
+					}
+				});
 			}
 		}
 	});
 
 	// Delete all users
 	app.post('/delete', function (req, res) {
+		console.log("Trying to delete all users");
+
 		db.run("DELETE FROM users", function(err, row) {
 			if (err != null){ res.send("Failed to delete all users"); }
 			else { res.send("All users deleted!"); }
